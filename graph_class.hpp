b39a7dd71ba3ffcee66a16941810ba9589a7995b
tomiczekf <filip4546@gmail.com>
2025-04-04 08:05:21 +0200

    lcd1

--- graph_class.hpp
+++ graph_class.hpp
@@ -12,7 +12,6 @@
 // **************************************************************************
 
 #include "lcd_lib.h"
-
 // Simple graphic interface
 
 struct Point2D 
@@ -38,20 +37,28 @@ public:
         m_fg_color( t_fg_color ), m_bg_color( t_bg_color ) {}
 
     // ONLY ONE INTERFACE WITH LCD HARDWARE!!!
-    void drawPixel( int32_t t_x, int32_t t_y ) { lcd_put_pixel( t_x, t_y, convert_RGB888_to_RGB565( m_fg_color ) ); }
     
     // Draw graphics element
     virtual void draw() = 0;
     
     // Hide graphics element
     virtual void hide() { swap_fg_bg_color(); draw(); swap_fg_bg_color(); }
+    void drawPixel( int32_t t_x, int32_t t_y ) { 
+        lcd_put_pixel( t_x, t_y, convert_RGB888_to_RGB565( m_fg_color ) ); 
+    }
 private:
     // swap foreground and backgroud colors
     void swap_fg_bg_color() { RGB l_tmp = m_fg_color; m_fg_color = m_bg_color; m_bg_color = l_tmp; } 
 
     // IMPLEMENT!!!
     // conversion of 24-bit RGB color into 16-bit color format
-    uint16_t convert_RGB888_to_RGB565( RGB t_color ) { return 0x07E0; /* green color */ }
+    uint16_t convert_RGB888_to_RGB565( RGB t_color ) { 
+        uint16_t r = (t_color.r >> 3) & 0x1F; // Extract top 5 bits of red
+        uint16_t g = (t_color.g >> 2) & 0x3F; // Extract top 6 bits of green
+        uint16_t b = (t_color.b >> 3) & 0x1F; // Extract top 5 bits of blue
+
+        return (r << 11) | (g << 5) | b; // Combine into RGB565 format
+    }// Combine into RGB565 format}
 };
 
 
@@ -78,8 +85,45 @@ public:
     Circle( Point2D t_center, int32_t t_radius, RGB t_fg, RGB t_bg ) : 
         GraphElement( t_fg, t_bg ), m_center( t_center ), m_radius( t_radius ) {}
 
-    void draw() { } // IMPLEMENT!!!
+    void draw() override 
+        {
+            int32_t x = 0;
+            int32_t y = m_radius;
+            int32_t d = 3 - 2 * m_radius;
+    
+            // Draw initial points
+            drawCirclePoints(m_center.x, m_center.y, x, y);
+            while (y >= x) 
+            {
+                x++;
+                if (d > 0) 
+                {
+                    y--;
+                    d = d + 4 * (x - y) + 10;
+                } 
+                else 
+                {
+                    d = d + 4 * x + 6;
+                }
+                drawCirclePoints(m_center.x, m_center.y, x, y);
+            }
+        }
+    
+    private:
+        // Helper function to draw symmetric points of the circle
+        void drawCirclePoints(int32_t xc, int32_t yc, int32_t x, int32_t y) 
+        {
+            drawPixel(xc + x, yc + y);
+            drawPixel(xc - x, yc + y);
+            drawPixel(xc + x, yc - y);
+            drawPixel(xc - x, yc - y);
+            drawPixel(xc + y, yc + x);
+            drawPixel(xc - y, yc + x);
+            drawPixel(xc + y, yc - x);
+            drawPixel(xc - y, yc - x);
+        }
 };
+
 
 
 class Character : public GraphElement 
@@ -92,8 +136,22 @@ public:
 
     Character( Point2D t_pos, char t_char, RGB t_fg, RGB t_bg ) : 
       GraphElement( t_fg, t_bg ), m_pos( t_pos ), m_character( t_char ) {};
-
-    void draw() { }; // IMPLEMENT!!!
+      void draw() override 
+      {
+          // Get the ASCII value of the character
+          int ascii_value = static_cast<int>(m_character);
+          // Draw the character using the font8x8 array
+          for (int i = 0; i < 8; i++) 
+          {
+              for (int j = 0; j < 8; j++) 
+              {
+                  if (font8x8[ascii_value][i] & (1 << (7 - j))) 
+                  {
+                      drawPixel(m_pos.x + j, m_pos.y + i);
+                  }
+              }
+          }
+      }
 };
 
 
@@ -106,7 +164,22 @@ public:
     Line( Point2D t_pos1, Point2D t_pos2, RGB t_fg, RGB t_bg ) : 
       GraphElement( t_fg, t_bg ), m_pos1( t_pos1 ), m_pos2( t_pos2 ) {}
 
-    void draw() {  }; // IMPLEMENT!!!
+    void draw() {
+        int32_t dx = m_pos2.x - m_pos1.x;
+        int32_t dy = m_pos2.y - m_pos1.y;
+        int32_t steps = std::max(abs(dx), abs(dy));
+        float xIncrement = static_cast<float>(dx) / steps;
+        float yIncrement = static_cast<float>(dy) / steps;
+
+        float x = m_pos1.x;
+        float y = m_pos1.y;
+
+        for (int i = 0; i <= steps; i++) {
+            drawPixel(static_cast<int32_t>(x), static_cast<int32_t>(y));
+            x += xIncrement;
+            y += yIncrement;
+        }
+    }; // IMPLEMENT!!!
 };
 
 
--- main-lcd-simul.cpp
+++ main-lcd-simul.cpp
@@ -23,7 +23,7 @@ int main()
 {
     lcd_init();                     // LCD initialization
 
-    uint16_t l_color_red = 0xF800;
+    /*uint16_t l_color_red = 0xF800;
     uint16_t l_color_green = 0x07E0;
     uint16_t l_color_blue = 0x001F;
     uint16_t l_color_white = 0xFFFF;
@@ -39,7 +39,75 @@ int main()
             lcd_put_pixel( ofs + i, ofs + l_limit, l_color_blue );
             lcd_put_pixel( ofs + l_limit, ofs + i, l_color_white );
         }
-    }
+    }*/
+    RGB red = {255, 0, 0};
+    RGB green = {0, 255, 0};
+    RGB blue = {0, 0, 255};
+    RGB white = {255, 255, 255};
+    RGB black = {0, 0, 0};
+
+    Point2D center = {LCD_WIDTH / 2, LCD_HEIGHT / 2};
+    int radius = 150;
+
+    Circle circle(center, radius, red, white);
+    circle.draw();
+
+    // Create a Character object
+    Point2D char_pos = {50, 50}; // Position on the screen
+    Character character(char_pos, 'X', white, black);
+    character.draw();
+
+
+    
+
+    // Define points for the tall rectangle
+    Point2D tall_top_left = {50, 50};
+    Point2D tall_top_right = {100, 50};
+    Point2D tall_bottom_left = {50, 200};
+    Point2D tall_bottom_right = {100, 200};
+
+    // Draw the tall rectangle
+    Line tall_top(tall_top_left, tall_top_right, red, white);
+    Line tall_bottom(tall_bottom_left, tall_bottom_right, red, white);
+    Line tall_left(tall_top_left, tall_bottom_left, red, white);
+    Line tall_right(tall_top_right, tall_bottom_right, red, white);
+
+    tall_top.draw();
+    tall_bottom.draw();
+    tall_left.draw();
+    tall_right.draw();
+
+    // Define points for the wide rectangle
+    Point2D wide_top_left = {150, 100};
+    Point2D wide_top_right = {300, 100};
+    Point2D wide_bottom_left = {150, 150};
+    Point2D wide_bottom_right = {300, 150};
+
+    // Draw the wide rectangle
+    Line wide_top(wide_top_left, wide_top_right, green, white);
+    Line wide_bottom(wide_bottom_left, wide_bottom_right, green, white);
+    Line wide_left(wide_top_left, wide_bottom_left, green, white);
+    Line wide_right(wide_top_right, wide_bottom_right, green, white);
+
+    wide_top.draw();
+    wide_bottom.draw();
+    wide_left.draw();
+    wide_right.draw();
+
+    // Draw diagonal lines for the tall rectangle
+    Line tall_diagonal1(tall_top_left, tall_bottom_right, blue, white);
+    Line tall_diagonal2(tall_top_right, tall_bottom_left, blue, white);
+
+    tall_diagonal1.draw();
+    tall_diagonal2.draw();
+
+    // Draw diagonal lines for the wide rectangle
+    Line wide_diagonal1(wide_top_left, wide_bottom_right, blue, white);
+    Line wide_diagonal2(wide_top_right, wide_bottom_left, blue, white);
+
+    wide_diagonal1.draw();
+    wide_diagonal2.draw();
+
 
     cv::imshow( LCD_NAME, g_canvas );   // refresh content of "LCD"
     cv::waitKey( 0 );                   // wait for key 
